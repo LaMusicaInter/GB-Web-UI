c37518e73b9d0315ab1580dae577f175b98ae4b6
Elisabeth <86415318+LaMusicaInter@users.noreply.github.com>
2022-04-16 16:46:27 +0300

    Update DegenerateTriangleException.java

--- hw4/src/main/java/ru/example/DegenerateTriangleException.java
+++ hw4/src/main/java/ru/example/DegenerateTriangleException.java
@@ -1,11 +1,5 @@
-package ru.iq_soft;
-/**
- * Класс DegenerateTriangleException
- *      Исключение на случай вырожденного треугольника в отрезок или точку
- * @author : Хильченко А.Н
- * @project : HW_4
- * @date : 11.02.2022
- */
+package org.example;
+
 
 public class DegenerateTriangleException extends Exception{
     public DegenerateTriangleException() {
