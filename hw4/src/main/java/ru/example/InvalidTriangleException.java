afc3cfd55c62bcdbd758da8e3e2a99a9460272c9
Elisabeth <86415318+LaMusicaInter@users.noreply.github.com>
2022-04-16 16:47:42 +0300

    Update InvalidTriangleException.java

--- hw4/src/main/java/ru/example/InvalidTriangleException.java
+++ hw4/src/main/java/ru/example/InvalidTriangleException.java
@@ -1,12 +1,5 @@
-/**
- * Класс InvalidTriangleException
- *      Исключение на случай невозможности треугольника херона по трем сторонам
- * @author : Хильченко А.Н
- * @project : HW_4
- * @date : 11.02.2022
- */
 
-package ru.iq_soft;
+package org.example;
 import java.util.*;
 
 public class InvalidTriangleException extends Exception{
